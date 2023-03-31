23b0aac596a2efe11a2d3d414a944250e888b865
sagedev2k16 <sagedev2k16@gmail.com>
2023-03-31 09:03:20 +0530

    Added middleware, db

--- index.js
+++ index.js
@@ -1,21 +1,82 @@
 import Express from "express";
 import path from 'path';
 import cors from 'cors';
+import helmet from 'helmet';
+import axios from 'axios';
+import knex from "knex";
 
 const app = Express();
 
+// Parse incoming JSON request body
 app.use(Express.json());
+
+// Use public/dist folder to serve static content
 app.use(Express.static(path.join("public", "dist")));
+
+// Allow cors requests
 app.use(cors());
 
+// Safeguard application with helmet
+app.use(helmet());
+
+// Use the PORT exposed by environment. If PORT not defined in env, then use 8775
 const PORT = process.env.PORT || 8775;
 
+// Define initial list of users
 let users = [
     {userName: "John", empId: 1, role: "employee"},
     {userName: "Luke", empId: 2, role: "manager"},
     {userName: "Jane", empId: 3, role: "manager"},
     {userName: "Leia", empId: 4, role: "employee"},
 ];
+
+const db = knex({
+    client: "sqlite3",
+    connection: {
+        filename: "./db.sqlite"
+    }
+});
+
+try {
+    await db.schema.createTable("users", table => {
+        table.increments('empId');
+        table.string('userName');
+        table.string('role')
+    });
+
+    await db('users').insert({userName: "John", role: "employee"});
+    await db('users').insert({userName: "Jane", role: "manager"});
+    await db('users').insert({userName: "Luke", role: "manager"});
+    await db('users').insert({userName: "Leia", role: "employee"});
+} catch(e) {
+    console.log(e);
+}
+
+// Using axios with express
+app.get("/apiData", async (req, res) => {
+    let apiResponse;
+    
+    try {
+        apiResponse = await axios.get("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json", {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+    
+        res.send(apiResponse.data);
+    } catch(e) {
+        res.send({
+            msg: "Error occurred",
+            error: e
+        });
+    }
+});
+
+// Express middleware
+app.use("*", (req, res, next) => {
+    console.log("Got request", req.method, req.originalUrl);
+    next();
+});
 
 app.get("/", (req, res) => {
     res.send("Hello from server");
@@ -55,6 +116,11 @@ app.post("/create/:type", (req, res) => {
 app.get("/users", (req, res) => {
     console.log("Getting users");
     res.send(users);
+});
+
+app.get("/db-users", async (req, res) => {
+    let dbUsers = await db.select().table('users');
+    res.send(dbUsers);
 });
 
 // Read a user with id
